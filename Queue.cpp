88980ec0a78ab33ab55739170156b758191cce46
Mostafa Ashraf <mostaphaashraf1996@gmail.com>
2020-02-19 16:31:52 +0200

    new virsion of implmenet using polymorphism concept and added linked implemention

--- Queue.cpp
+++ Queue.cpp
@@ -1,18 +1,22 @@
 #include "Queue.h"
+
+/*
+	Queue Array Implemention
+*/
 template<class T>
-Queue<T>::Queue()
+QueueA<T>::QueueA()
 {
 	front = 0;
 	rear = -1;
 	size = 0;
 }
 template<class T>
-void Queue<T>::append(T val)
+void QueueA<T>::append(T val)
 {
 	/*
-		using concept of circular array 
-		to avoid problem of moving elements in new array 
-		each time retrive or append element
+	using concept of circular array
+	to avoid problem of moving elements in new array
+	each time retrive or append element
 	*/
 	if (rear == ARRAY_SIZE - 1 && size < ARRAY_SIZE)
 		rear = -1;
@@ -23,7 +27,7 @@ void Queue<T>::append(T val)
 	}
 }
 template<class T>
-T Queue<T>::retrive()
+T QueueA<T>::retrive()
 {
 	/*
 	using concept of circular array
@@ -34,14 +38,68 @@ T Queue<T>::retrive()
 		return -1;
 	else
 	{
-		size--;	
+		size--;
 		int temp = block[front];
 		front = (front + 1) % ARRAY_SIZE;
 		return temp;
 	}
 }
 template<class T>
-int Queue<T>  ::size()
+int QueueA<T>  ::QSize()
 {
 	return size;
 }
+
+/*******
+			end of Queue array implemntion
+				*********/
+
+
+
+/***********
+	Queue linked list implemention
+		************/
+template<class T>
+int QueueL<T> ::QSize() {
+	return size;
+}
+template<class T>
+QueueL<T>::QueueL()
+{
+	head = NULL;
+	tail = NULL;
+	size = 0;
+}
+
+template<class T>
+void QueueL<T> ::append(T val)
+{
+	noode<T> *pn = (noode<T>*)new noode<T>;
+	pn->value = val;
+	pn->next = NULL;
+	if (!tail)
+	{
+		head = pn;
+	}
+	else
+	{
+		tail->next = pn;
+	}
+	tail = pn;
+	size++;
+}
+
+template<class T>
+T QueueL<T> ::retrive() {
+	noode<T> *pn = (noode<T>*)new noode<T>;
+	if (!head) return -1;
+	pn = head;
+	head = head->next;
+	T temp = pn->value;
+	delete pn;
+	if (!head)
+		tail = NULL;
+	size--;
+	return temp;
+}
+
--- Queue.h
+++ Queue.h
@@ -2,23 +2,93 @@
 #define QUEUE_H
 
 #define ARRAY_SIZE 100
-/*************** implement queue using circular array to avoid moveing each time *********************/
+
 /*
-	i'm using a object oriented programming conceptes like teamplates and another concepts 
-	in implemntion
+	using in implemention 
+	object oriented programming conceptes 
+	like (polymorphism,
+	teamplates, ....) 
+	and another concepts 
 */
+
 template<class T>
 class Queue
 {
+	/*
+	Abstract base class
+	*/
+public:
+
+	virtual void append(T val) = 0;
+	virtual T retrive() = 0;
+	virtual int QSize() = 0;
+};
+
+template<class T>
+class QueueA : public Queue<T>
+{
+	/*
+	Drived Class
+	*/
+	/********
+	implement QueueA related to Araay 
+	using "Circular Array Concept"
+	to avoid moveing each time appednd or retrive
+	*/
 	T block[ARRAY_SIZE];
 	int front, rear, size;
+public:
+	QueueA();
+	void append(T val);
+	T retrive();
+	int QSize();
+};
+
+/***********
+linked list implemention
+************/
+/*template<class T>
+class noode
+{
+public:
+	noode<T> *next;
+	T value;
+};
+template<class T>
+class Queue
+{
+private:
+	noode<T> * head;
+	noode<T> *tail;
+	int size;
 public:
 	Queue();
 	void append(T val);
 	T retrive();
-private:
+	int QSize();
+};*/
 
+template<class T>
+class noode
+{
+public:
+	noode<T> *next;
+	T value;
 };
-
-
+template<class T>
+class QueueL : public Queue<T>
+{
+	/*
+	Drived Class
+	*/
+private:
+	noode<T> * head;
+	noode<T> *tail;
+	int size;
+public:
+	QueueL();
+	void append(T val);
+	T retrive();
+	int QSize();
+};
 #endif
